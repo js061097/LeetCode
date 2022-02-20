afc3c809c1e62b7939a385d4911fc76343bd84b9
Jason Stanley <js41074546@gmail.com>
2022-02-20 15:33:29 -0600

    Create pldrnum.cpp

---
+++ 9. Palindrome Number/pldrnum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x==0)
+            return true;
+        if(x<0 || x%10 ==0)
+            return false;
+        int reverse = 0;
+        while(x>reverse){
+            reverse = reverse*10 + x%10;
+            x = x/10;
+        }
+        if(x==reverse)
+            return true;
+        else if(x==(reverse/10))
+            return true;
+        else 
+            return false;
+    }
+};
