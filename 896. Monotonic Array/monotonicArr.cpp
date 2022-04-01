203d7e785ee728bd66745ba6ddcf4a95482b47be
Jason Stanley <js41074546@gmail.com>
2022-03-31 21:26:28 -0500

    Create monotonicArr.cpp

---
+++ 896. Monotonic Array/monotonicArr.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true,decreasing = true;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<nums[i-1])
+                increasing = false;
+            else if(nums[i]>nums[i-1])
+                decreasing = false;
+        }
+        return increasing || decreasing;
+    }
+};
+
